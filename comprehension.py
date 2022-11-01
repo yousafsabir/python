f18c8b710b55f6fda67a6536a71e0b7d097942be
yousafsabir <yousafsabir430516@gmail.com>
2022-11-01 15:04:50 +0500

    comprehensions and functions

---
+++ comprehension.py
@@ -0,0 +1,11 @@
+### Comprehension :- One line initialization of list, tuples, dictionaries
+
+# initializing 10 element list
+a_list = [x for x in range(10)]
+print(a_list)
+
+b_list = [x + 5 for x in range(10)] # add 5 in each element
+print(b_list)
+
+c_list = [x for x in range(5, 51, 5)] # 5 table
+print(c_list)
---
+++ functions.py
@@ -0,0 +1,9 @@
+def func():
+    print("hello world")
+    def funct2():
+        print("hello again")
+    funct2()
+
+func()
+
+# can also define functions inside of functions in python
